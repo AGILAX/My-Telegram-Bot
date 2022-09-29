627cddcc45bf491ce5ebe446aab830ffb1ba1b54
Ahmed <113825113+AGILAX@users.noreply.github.com>
2022-09-29 17:38:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ bot = telebot.TeleBot('5412971012:AAED2iKNBl9tdCzRuDwHh0dKDMAjUBHpZ9s')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
+    mess = f'Hi, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
     if message.text == 'Hello':
-        bot.send_message(message.chat.id, 'И тебе привет!', parse_mode='html')
+        bot.send_message(message.chat.id, 'Hi', parse_mode='html')
     elif message.text == 'id':
         bot.send_message(message.chat.id, f'Твой ID: {message.from_user.id}', parse_mode='html')
     elif message.text == 'photo':
-        photo = open('Типы данных Python.PNG', 'rb')
+        photo = open('Types of data Python.PNG', 'rb')
         bot.send_photo(message.chat.id, photo)
     else:
-        bot.send_message(message.chat.id, 'Я тебя не понимаю (', parse_mode='html')
+        bot.send_message(message.chat.id, 'i dont understand you (', parse_mode='html')
 
 
 @bot.message_handler(content_types=['photo'])
